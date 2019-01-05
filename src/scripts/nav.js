1288908fe9f221ed55e5404eeb769e2f5c0f3289
Meg Cruzen <megcruzen@gmail.com>
2019-01-05 09:15:56 -0600

    Added navigation bar

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,5 +1,26 @@
 const nav = {
+    createNav() {
+        let navContainer = document.querySelector("#navigation");
 
+        let logo = document.createElement("div");
+        logo.setAttribute("class", "logo");
+        logo.innerHTML = "<a href='#'>Betsy's Boutique</a>"
+
+        let spacer = document.createElement("div");
+
+        let navLinks = document.createElement("div");
+        navLinks.setAttribute("class", "navlinks");
+        navLinks.innerHTML = `<ul>
+                <li><a href="#">Categories</a></li>
+                <li><a href="#">Orders</a></li>
+                <li><a href="#">Log Out</a></li>
+            </ul>`;
+
+        navContainer.appendChild(logo);
+        navContainer.appendChild(spacer);
+        navContainer.appendChild(navLinks);
+
+    }
 }
 
 export default nav;
